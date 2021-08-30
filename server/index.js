53c95cb4f9c548917ade948625438e9912a7190e
Rubanda <rubanda57@gmail.com>
2021-08-30 17:04:54 +0300

    Added the cors middleware

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const cors = require("cors")
 const classesRouter = require("./routers/classes")
 const studentsRouter = require("./routers/students")
 const coursesRouter = require("./routers/courses")
@@ -10,6 +10,7 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
